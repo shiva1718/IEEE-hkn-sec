3921a664ba42f52022dd26cfec33eafde5d104b8
Vishwaraviraaj08 <vishwaraviraaj08@gmail.com>
2023-11-04 09:38:18 +0530

    about

--- components/About/AboutSectionTwo.tsx
+++ components/About/AboutSectionTwo.tsx
@@ -7,17 +7,17 @@ const AboutSectionTwo = () => {
         <div className="-mx-4 flex flex-wrap items-center">
           <div className="w-full px-4 lg:w-1/2">
             <div
-              className="wow fadeInUp relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
+              className="wow fadeInUp relative mx-auto mb-12 aspect-[25/20] max-w-[500px] text-center lg:m-0"
               data-wow-delay=".15s"
             >
               <Image
-                src="/images/about/SUMMA.png"
+                src="/images/about/SUMMA-black.png"
                 alt="about image"
                 fill
                 className="drop-shadow-three dark:hidden dark:drop-shadow-none"
               />
               <Image
-                src="/images/about/SUMMA.png"
+                src="/images/about/SUMMA-white.png"
                 alt="about image"
                 fill
                 className="drop-shadow-three hidden dark:block dark:drop-shadow-none"
