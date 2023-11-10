867ba3b1aba8fa40dba38f8214ad2da09ed5c78c
Shiva Sankar <ms.shiva2019@gmail.com>
2023-11-10 18:05:53 +0530

    use light mode as default

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="light">
       {children}
     </ThemeProvider>
   );
